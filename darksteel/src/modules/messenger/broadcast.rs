c8885f0ac4dfb9c94ff7ada9c2330c2603cf38e8
Reese van Dijk <exadyne@protonmail.com>
2021-09-30 17:29:01 +1000

    Bump tokio version. Add Mpsc messenger module. Rename Messenger to Broadcast.

---
+++ darksteel/src/modules/messenger/broadcast.rs
@@ -0,0 +1,61 @@
+use super::error::*;
+use crate::modules::{IntoModule, Modules};
+use std::any::{Any, TypeId};
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::sync::Mutex;
+use tokio::sync::broadcast::*;
+use unchecked_unwrap::UncheckedUnwrap;
+
+const MESSAGE_QUEUE_CHANNEL_SIZE: usize = 256;
+
+#[derive(Debug, Clone)]
+/// A messenger module.
+pub struct Broadcast {
+    handles: Arc<Mutex<HashMap<TypeId, Box<dyn Any + Send + Sync + 'static>>>>,
+    capacity: usize,
+}
+
+impl Broadcast {
+    /// Create a new messenger.
+    pub fn new(capacity: usize) -> Self {
+        Self {
+            handles: Arc::new(Mutex::new(HashMap::new())),
+            capacity,
+        }
+    }
+
+    /// Get a broadcast channel for a given type
+    pub fn channel<T: Any + Send + Sync + Clone + 'static>(
+        &self,
+    ) -> Result<(Sender<T>, Receiver<T>), MessageError> {
+        match self.handles.lock() {
+            Ok(mut map) => {
+                if let Some(handle) = map.get(&TypeId::of::<T>()) {
+                    // This is completely safe. We never create an object
+                    // without a TypeId as the index.
+                    let sender = unsafe { handle.downcast_ref::<Sender<T>>().unchecked_unwrap() };
+                    let handle_sender = sender.clone();
+                    let handle_receiver = sender.subscribe();
+
+                    Ok((handle_sender, handle_receiver))
+                } else {
+                    let (sender, _) = channel::<T>(self.capacity);
+                    let handle_sender = sender.clone();
+                    let handle_receiver = sender.subscribe();
+                    map.insert(TypeId::of::<T>(), Box::new(sender));
+
+                    Ok((handle_sender, handle_receiver))
+                }
+            }
+            Err(_) => Err(MessageError::LockPoisoned),
+        }
+    }
+}
+
+#[crate::async_trait]
+impl IntoModule for Broadcast {
+    async fn module(_: &Modules) -> Self {
+        Broadcast::new(MESSAGE_QUEUE_CHANNEL_SIZE)
+    }
+}
---
+++ darksteel/src/modules/messenger/mpsc.rs
@@ -0,0 +1,93 @@
+use super::error::*;
+use crate::modules::{IntoModule, Modules};
+use std::any::{Any, TypeId};
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::sync::Mutex;
+use tokio::sync::{mpsc::*, Mutex as AsyncMutex, MutexGuard};
+use unchecked_unwrap::UncheckedUnwrap;
+
+#[derive(Clone)]
+pub struct OwnedReceiver<T>
+where
+    T: Any + Send + Sync + Clone + 'static,
+{
+    rx: Arc<AsyncMutex<UnboundedReceiver<T>>>,
+}
+
+impl<T> OwnedReceiver<T>
+where
+    T: Any + Send + Sync + Clone + 'static,
+{
+    pub fn new(receiver: UnboundedReceiver<T>) -> Self {
+        Self {
+            rx: Arc::new(AsyncMutex::new(receiver)),
+        }
+    }
+
+    pub async fn handle(&self) -> MutexGuard<'_, UnboundedReceiver<T>> {
+        self.rx.lock().await
+    }
+
+    pub async fn drain(&self) {
+        let mut receiver = self.rx.lock().await;
+
+        loop {
+            if let Err(_) = receiver.try_recv() {
+                break;
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+/// A messenger module.
+pub struct Mpsc {
+    handles: Arc<Mutex<HashMap<TypeId, Box<dyn Any + Send + Sync + 'static>>>>,
+}
+
+impl Mpsc {
+    /// Create a new messenger.
+    pub fn new() -> Self {
+        Self {
+            handles: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+
+    /// Get a broadcast channel for a given type
+    pub fn channel<T: Any + Send + Sync + Clone + 'static>(
+        &self,
+    ) -> Result<(UnboundedSender<T>, OwnedReceiver<T>), MessageError> {
+        match self.handles.lock() {
+            Ok(mut map) => {
+                if let Some(handle) = map.get(&TypeId::of::<T>()) {
+                    // This is completely safe. We never create an object
+                    // without a TypeId as the index.
+                    let (sender, receiver) = unsafe {
+                        handle
+                            .downcast_ref::<(UnboundedSender<T>, OwnedReceiver<T>)>()
+                            .unchecked_unwrap()
+                    };
+                    Ok((sender.clone(), receiver.clone()))
+                } else {
+                    let (sender, receiver) = unbounded_channel::<T>();
+                    let handle_receiver = OwnedReceiver::<T>::new(receiver);
+                    map.insert(
+                        TypeId::of::<T>(),
+                        Box::new((sender.clone(), handle_receiver.clone())),
+                    );
+
+                    Ok((sender, handle_receiver))
+                }
+            }
+            Err(_) => Err(MessageError::LockPoisoned),
+        }
+    }
+}
+
+#[crate::async_trait]
+impl IntoModule for Mpsc {
+    async fn module(_: &Modules) -> Self {
+        Mpsc::new()
+    }
+}
